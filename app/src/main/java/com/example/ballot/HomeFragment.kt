2b93db765e107ff82795a47d50b2954903a02835
Branden Woodard <branden182@gmail.com>
2019-12-02 13:41:24 -0500

    some progress on fragment_home.xml

--- app/src/main/java/com/example/ballot/HomeFragment.kt
+++ app/src/main/java/com/example/ballot/HomeFragment.kt
@@ -1,18 +1,13 @@
 package com.example.ballot
 
 
-import android.app.PendingIntent.getActivity
-import android.app.PendingIntent.writePendingIntentOrNullToParcel
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.fragment_home.*
-import org.w3c.dom.Text
 
 /**
  * A simple [Fragment] subclass.
@@ -31,7 +26,7 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         // Instead of view.findViewById(R.id.hello) as TextView
-        t1?.setOnClickListener {
+        bill1?.setOnClickListener {
             val intent = Intent(activity, BillSummary::class.java)
             activity!!.startActivity(intent)
         }
