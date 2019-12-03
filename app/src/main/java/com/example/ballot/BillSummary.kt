6bf87d9135fef6b016cca4549f107b0eb11fd4b8
kelliw <kwebber1@terpmail.umd.edu>
2019-12-03 08:48:19 -0500

    bills screen

--- app/src/main/java/com/example/ballot/BillSummary.kt
+++ app/src/main/java/com/example/ballot/BillSummary.kt
@@ -1,9 +1,10 @@
 package com.example.ballot
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-
 import kotlinx.android.synthetic.main.activity_bill_summary.*
 
 class BillSummary : AppCompatActivity() {
@@ -12,7 +13,19 @@ class BillSummary : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bill_summary)
 
-
+        back_arrow?.setOnClickListener {
+            val quitIntent = Intent(this@BillSummary, MainActivity::class.java)
+            startActivity(quitIntent)
+        }
     }
+
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//        // Instead of view.findViewById(R.id.hello) as TextView
+//        back_arrow?.setOnClickListener {
+//            val intent = Intent(activity, HomeFragment::class.java)
+//            activity!!.startActivity(intent)
+//        }
+
 
 }
